95c258b76b5f6bb8a9cf1ca92af7fe315a79c515
100mil <>
2019-06-16 20:16:03 -0400

    receiver functions

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "King of Hearts")
+
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Spades"
+}
