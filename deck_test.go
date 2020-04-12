174ceecce09313f236166d08f3d67ff6bb289aa5
Saumil Gandhi <6945144+saumilg66@users.noreply.github.com>
2020-04-12 19:57:55 -0400

    file i/o test

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -12,4 +15,19 @@ func TestNewDeck(t *testing.T) {
 	if d[0] != "1 of Spades" {
 		t.Errorf("Expected 1 of spades, but got %v", d[0])
 	}
+}
+
+func TestSaveToFileAndReadFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := readFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
